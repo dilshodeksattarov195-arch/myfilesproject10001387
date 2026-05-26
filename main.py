310785e1fe4979fe4b72e086d11cea5258bbd5a0
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 01:49:42 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class sessionDenderWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = sessionDenderWorker()
+    print("Worker engine initialized.")
